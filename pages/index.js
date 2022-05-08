f0079a068790737517a728ff0dc94cb9766aff5b
Harry-Yates <hyates1@gmail.com>
2022-05-08 18:52:34 +0200

    mocked up home screen

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,61 @@
+import React from "react";
 import BaseLayout from "../components/layout/BaseLayout";
-import { Container, Button } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
+import Image from "next/image";
 
-const Home = () => {
-    return (
-        <BaseLayout>
-            <Container>
-                <Button color='danger'>Danger</Button>
-
-                <div className='parent-class siblin-class'>
-                    <h1>Harry YAtes</h1>
-                    <h2>Hello</h2>
-                    <h3>Hello</h3>
-                    <div className='child-class'>Hello World</div>
+class Index extends React.Component {
+    render() {
+        return (
+            <BaseLayout className='cover'>
+                <div className='main-section'>
+                    <div className='background-image'>
+                        {/* <Image src='' alt='' width={1500} height={500} /> */}
+                    </div>
+                    <Container>
+                        <Row>
+                            <Col md='6'>
+                                <div className='hero-section'>
+                                    <div className={`flipper`}>
+                                        <div className='back'>
+                                            <div className='hero-section-content'>
+                                                <h2> Frontend Developer </h2>
+                                                <div className='hero-section-content-intro'>
+                                                    Have a look at my portfolio and job
+                                                    history.
+                                                </div>
+                                            </div>
+                                            <Image
+                                                className='image'
+                                                src='/images/section-1.png'
+                                                alt='Alt Home picture'
+                                                width={500}
+                                                height={600}
+                                            />
+                                            <div className='shadow-custom'>
+                                                <div className='shadow-inner'> </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Col>
+                            <Col md='6' className='hero-welcome-wrapper'>
+                                <div className='hero-welcome-text'>
+                                    <h2>
+                                        Welcome to the portfolio of Harry Yates. A
+                                        Frontend Developer based between Stockholm and
+                                        London.
+                                    </h2>
+                                </div>
+                                <div className='hero-welcome-bio'>
+                                    <h3>Selected Work</h3>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Container>
                 </div>
-            </Container>
-        </BaseLayout>
-    );
-};
+            </BaseLayout>
+        );
+    }
+}
 
-export default Home;
+export default Index;
